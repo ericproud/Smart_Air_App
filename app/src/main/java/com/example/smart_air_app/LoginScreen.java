7ccda0fe40f821c3601e063969a44392a6b1c7c3
SleepyLulu <68202582+sleepylulu7@users.noreply.github.com>
2025-11-11 17:34:00 -0500

    feat: added Login & Register screens and switching between them

--- app/src/main/java/com/example/smart_air_app/LoginScreen.java
+++ app/src/main/java/com/example/smart_air_app/LoginScreen.java
@@ -1,46 +1,35 @@
 package com.example.smart_air_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.Button;
-import android.view.View;
-import android.content.Intent;
 
-
-public class StartScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start_screen);
+        setContentView(R.layout.activity_login_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button loginButton = findViewById(R.id.button_login);
+        Button backButton = findViewById(R.id.buttonLoginBack);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(StartScreen.this, LoginScreen.class);
-                startActivity(intent);
-            }
-        });
-
-        Button registerButton = findViewById(R.id.button_register);
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(StartScreen.this, RegisterScreen.class);
+                Intent intent = new Intent(LoginScreen.this, StartScreen.class);
                 startActivity(intent);
             }
         });
