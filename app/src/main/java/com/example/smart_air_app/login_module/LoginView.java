127da26629eb28d400ea4b7c02a907e2030212c5
ericp <eric.proud11@gmail.com>
2025-11-19 15:35:01 -0500

    feat: Users can now login to their account

--- app/src/main/java/com/example/smart_air_app/login_module/LoginView.java
+++ app/src/main/java/com/example/smart_air_app/login_module/LoginView.java
@@ -1,9 +1,13 @@
 package com.example.smart_air_app.login_module;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +15,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.smart_air_app.ChildHomeScreen;
+import com.example.smart_air_app.DoctorHomeScreen;
+import com.example.smart_air_app.ParentHomeScreen;
 import com.example.smart_air_app.R;
 import com.example.smart_air_app.StartScreen;
 
-public class LoginView extends AppCompatActivity {
+public class LoginView extends AppCompatActivity implements LoginPresenter.AuthView {
+    Button backButton;
+    Button loginButton;
+    Button recoverAccountButton;
+    EditText inputUsernameOrEmail;
+    EditText inputPassword;
+    TextView authFailedText;
+    LoginPresenter presenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +42,23 @@ public class LoginView extends AppCompatActivity {
             return insets;
         });
 
-        Button backButton = findViewById(R.id.buttonLoginBack);
+        backButton = findViewById(R.id.buttonLoginBack);
+        loginButton = findViewById(R.id.LoginButton);
+        inputUsernameOrEmail = findViewById(R.id.inputLoginUsernameOrEmail);
+        inputPassword = findViewById(R.id.inputLoginPassword);
+        authFailedText = findViewById(R.id.AuthFailedText);
+        presenter = new LoginPresenter(new LoginModel(), this);
+
+        authFailedText.setVisibility(View.INVISIBLE);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputUsernameOrEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                presenter.Login(email, password);
+            }
+        });
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,4 +68,47 @@ public class LoginView extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public void redirectToHome(String userType) {
+        Intent intent;
+
+        if (userType.equals("child")) {
+            intent = new Intent(LoginView.this, ChildHomeScreen.class);
+        }
+        else if (userType.equals("parent")) {
+            intent = new Intent(LoginView.this, ParentHomeScreen.class);
+        }
+        else if (userType.equals("doctor")) {
+            intent = new Intent(LoginView.this, DoctorHomeScreen.class);
+        }
+        else intent = new Intent(LoginView.this, StartScreen.class);
+        startActivity(intent);
+        finish();
+    };
+
+    @Override
+    public void showAuthFailedMessage() {
+        authFailedText.setVisibility(View.VISIBLE);
+    };
+
+    @Override
+    public void resetInputs(){
+        inputUsernameOrEmail.setText("");
+        inputPassword.setText("");
+    };
+
+    @Override
+    public void showError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+    };
+
+    @Override
+    public void onEmptyUsernameOrEmail() {
+        inputUsernameOrEmail.setHintTextColor(getResources().getColor(android.R.color.holo_red_dark));
+    };
+
+    @Override
+    public void onEmptyPassword() {
+        inputPassword.setHintTextColor(getResources().getColor(android.R.color.holo_red_dark));
+    };
 }
