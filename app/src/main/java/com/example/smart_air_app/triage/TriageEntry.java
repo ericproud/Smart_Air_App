3f791f6e17e6c3111363c4eaab9bee6ec0f1c1d9
SleepyLulu <68202582+sleepylulu7@users.noreply.github.com>
2025-11-19 00:17:05 -0500

    feat: triage entries now assigned ID based on number of triages, and getter/setter methods added

--- app/src/main/java/com/example/smart_air_app/triage/TriageEntry.java
+++ app/src/main/java/com/example/smart_air_app/triage/TriageEntry.java
@@ -2,18 +2,51 @@ package com.example.smart_air_app.triage;
 
 public class TriageEntry {
 
+    private static int numTriages = 0; // Number of total triage entries across all users
     private int triageID;
     private boolean[] redFlags = new boolean[3];
+    // redFlags[0]: can't speak full sentences
+    // redFlags[1]: chest pulling in/retractions
+    // redFlags[2]: blue/gray lips or nails
     private boolean recentRescue;
     private double PEF;
 
     public TriageEntry() {
-        triageID = -1;
+        triageID = numTriages;
+        numTriages++;
         redFlags[0] = false;
         redFlags[1] = false;
         redFlags[2] = false;
         recentRescue = false;
         PEF = -1;
+    }
+
+    public int getTriageID() {
+        return triageID;
+    }
+
+    public void setRedFlag(int flagNum, boolean value) {
+        redFlags[flagNum] = value;
+    }
+
+    public boolean getRedFlag(int flagNum) {
+        return redFlags[flagNum];
+    }
+
+    public boolean getRecentRescue() {
+        return recentRescue;
+    }
+
+    public void setRecentRescue(boolean value) {
+        recentRescue = value;
+    }
+
+    public double getPEF() {
+        return PEF;
+    }
+
+    public void setPEF(double value) {
+        PEF = value;
     }
 
 }
