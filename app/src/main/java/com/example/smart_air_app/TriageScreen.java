583296f285cd2db28184d1894e8912075b7dea50
SleepyLulu <68202582+sleepylulu7@users.noreply.github.com>
2025-11-19 21:41:31 -0500

    feat: emergency button enables when any red flag is checked

--- app/src/main/java/com/example/smart_air_app/TriageScreen.java
+++ app/src/main/java/com/example/smart_air_app/TriageScreen.java
@@ -1,12 +1,15 @@
 package com.example.smart_air_app;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.chip.ChipGroup;
 
 public class TriageScreen extends AppCompatActivity {
 
@@ -19,6 +22,19 @@ public class TriageScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ChipGroup redFlagsGroup = findViewById(R.id.redFlagsGroup);
+        Button emergencyBtn = findViewById(R.id.btnEmergency);
+
+        redFlagsGroup.setOnCheckedStateChangeListener((chipGroup, checkedIds) -> {
+            boolean emergencyStatus = false; // true when one of the red flags is checked
+            if (checkedIds.isEmpty()) {
+                emergencyStatus = false;
+            } else {
+                emergencyStatus = true;
+            }
+            emergencyBtn.setEnabled(emergencyStatus); // enable emergency btn when red flag checked
         });
     }
 }
