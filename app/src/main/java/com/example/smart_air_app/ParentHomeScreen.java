9cb0933cc68bdaa586e2387e3643e4f512201c29
ericp <eric.proud11@gmail.com>
2025-11-22 13:10:29 -0500

    feat: Replaced placeholder buttons with a dynamic layout of buttons where each one will take a parent to a specific childs account

--- app/src/main/java/com/example/smart_air_app/ParentHomeScreen.java
+++ app/src/main/java/com/example/smart_air_app/ParentHomeScreen.java
@@ -9,29 +9,63 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.smart_air_app.session.SessionManager;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParentHomeScreen extends AppCompatActivity {
+    String parentUID;
+    DatabaseReference dbRef;
+    List<String> childUIDs;
+    LinearLayout container;
+    Button addChildButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_home_screen);
 
-        LinearLayout container = findViewById(R.id.childButtonContainer);
+        container = findViewById(R.id.childButtonContainer);
+        addChildButton = findViewById(R.id.addChildButton);
 
-        Button addChildButton = findViewById(R.id.addChildButton);
+        parentUID = SessionManager.getInstance().getUserId();
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        childUIDs = new ArrayList<>();
 
         addChildButton.setOnClickListener(v -> {
             Intent intent = new Intent(ParentHomeScreen.this, AddChildScreen.class);
             startActivity(intent);
         });
 
-        // Making placeholder buttons where the child accounts would go
-        for (int i=0; i< 4;i++) {
-            int childId = i;
+        dbRef.child(parentUID).child("children").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                    childUIDs.add(childSnapshot.getKey());
+                }
+                createChildButtons(childUIDs);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+    void createChildButtons(List<String> childUIDs) {
+        for (String childUID : childUIDs) {
             Button button = new Button(this);
-            button.setText("Child" + i + 1);
+
+            String childName = dbRef.child(childUID).child("firstName") + " " + dbRef.child(childUID).child("lastName");
+            button.setText(childName);
             button.setTextColor(Color.WHITE);
 
             GradientDrawable drawable = new GradientDrawable();
@@ -49,22 +83,17 @@ public class ParentHomeScreen extends AppCompatActivity {
             button.setLayoutParams(params);
 
             button.setClickable(true);
-            button.setOnClickListener(v ->
-                    Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
-            );
 
             button.setOnClickListener(v -> {
-                Intent intent = new Intent(ParentHomeScreen.this, ChildHomeScreen.class);
-
-                intent.putExtra("childId", childId);
-
+                Intent intent = new Intent(ParentHomeScreen.this, ParentChildHomeScreen.class);
+                intent.putExtra("childUID", childUID);
+                intent.putExtra("childName", childName);
                 startActivity(intent);
             });
 
             container.addView(button);
         }
     }
-
     private int dpToPx(int dp){
         return (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
