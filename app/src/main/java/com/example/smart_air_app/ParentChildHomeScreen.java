d90dc5549705915af28ceca123a6b9cf9d0b3f2c
ericp <eric.proud11@gmail.com>
2025-11-23 14:18:00 -0500

    feat: permission management for a parent to choose child account sharing

--- app/src/main/java/com/example/smart_air_app/ParentChildHomeScreen.java
+++ app/src/main/java/com/example/smart_air_app/ParentChildHomeScreen.java
@@ -10,7 +10,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ParentChildHomeScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
