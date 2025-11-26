fc165046ddd01ebc2ff9ad1802baa39c86579143
ericp <eric.proud11@gmail.com>
2025-11-26 15:16:30 -0500

    feat: spinner for choosing the children, and working calls to the database for one time code

--- app/src/main/java/com/example/smart_air_app/DoctorHomeScreen.java
+++ app/src/main/java/com/example/smart_air_app/DoctorHomeScreen.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -37,70 +38,98 @@ public class DoctorHomeScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-            Spinner patientSpinner = findViewById(R.id.patientSpinner);
+            patientSpinner = findViewById(R.id.patientSpinner);
+            patientName = findViewById(R.id.patientName);
+            enterOTC = findViewById(R.id.enterOTC);
+            submitOTCButton = findViewById(R.id.submitOTCButton);
 
-            String UID = FirebaseAuth.getInstance().getUid();
-            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Users");
-            ArrayList<String> patientUIDs = new ArrayList<>();
-            ArrayList<String> patientNames = new ArrayList<>();
-            HashMap<String, String> patientNameToUID = new HashMap<>();
+            // Get patient names from db, make array mapping patient name to patient UID
+            getPatientUIDs();
+            setUpOTC();
 
-            // Get patient UIDs from children linked to doctor from db
-            dbRef.child(UID).child("Patients").addListenerForSingleValueEvent(new ValueEventListener() {
+            return insets;
+        });
+    }
+
+    Spinner patientSpinner;
+    TextView patientName;
+    EditText enterOTC;
+    Button submitOTCButton;
+
+    String UID = FirebaseAuth.getInstance().getUid();
+    DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+    ArrayList<String> patientUIDs = new ArrayList<>();
+    ArrayList<String> patientNames = new ArrayList<>();
+    HashMap<String, String> patientNameToUID = new HashMap<>();
+
+    void getPatientUIDs() {
+        // Get patient UIDs from children linked to doctor from db
+        dbRef.child("Users").child(UID).child("Patients").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                    patientUIDs.add(childSnapshot.getKey());
+                }
+                getPatientNames();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    void getPatientNames() {
+        for (String patientUID : patientUIDs) {
+            dbRef.child("Users").child(patientUID).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot snapshot) {
-                    for (DataSnapshot childSnapshot : snapshot.getChildren()) {
-                        patientUIDs.add(childSnapshot.getKey());
+                    String firstName = snapshot.child("firstName").getValue(String.class);
+                    String lastName = snapshot.child("lastName").getValue(String.class);
+                    String patientName = firstName + " " + lastName;
+                    if (firstName != null && lastName != null) {
+                        patientNames.add(patientName);
+                        patientNameToUID.put(patientName, patientUID);
+                    }
+                    if (patientNames.size() == patientUIDs.size()) {
+                        setUpSpinner();
                     }
                 }
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
                 }
             });
+        }
+    }
 
-            // Get patient names from db, make array mapping patient name to patient UID
-            for (String patientUID : patientUIDs) {
-                dbRef.child(patientUID).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot snapshot) {
-                        for (DataSnapshot childSnapshot : snapshot.getChildren()) {
-                            String firstName = childSnapshot.child("firstName").getValue(String.class);
-                            String lastName = childSnapshot.child("lastName").getValue(String.class);
-                            String patientName = firstName + " " + lastName;
-                            patientNames.add(patientName);
-                            patientNameToUID.put(patientName, patientUID);
-                        }
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                }
-            });
+    void setUpSpinner() {
+        // Create dropdown menu and set it so that when a patient is selected, it sets the behavior of the buttons to access patient records
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, patientNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        patientSpinner.setAdapter(adapter);
+        patientSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String currentPatientName = patientSpinner.getSelectedItem().toString();
+                String currentPatientUID = patientNameToUID.get(currentPatientName);
+
+                // Setting patient name text
+                patientName.setText(currentPatientName);
+
+                // Setting Button functionality
+                setButtons(currentPatientName, currentPatientUID);
             }
-
-            // Create dropdown menu and set it so that when a patient is selected, it sets the behavior of the buttons to access patient records
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, patientNames);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            patientSpinner.setAdapter(adapter);
-            patientSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                    String currentPatientName = patientSpinner.getSelectedItem().toString();
-                    String currentPatientUID = patientNameToUID.get(currentPatientName);
-                    setButtons(currentPatientName, currentPatientUID);
-                }
-                @Override
-                public void onNothingSelected(AdapterView<?> parentView) {
-                }
-            });
-            return insets;
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
         });
     }
+
     void setButtons(String patientName, String patientUID){
         Button controllerAdherenceButton = findViewById(R.id.controllerAdherenceButton);
         Button rescueLogsButton = findViewById(R.id.rescueLogsButton);
         Button triageIncidentsButton = findViewById(R.id.triageIncidentsButton);
         Button summaryChartsButton = findViewById(R.id.summaryChartsButton);
-
+        /*
         controllerAdherenceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -140,6 +169,34 @@ public class DoctorHomeScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        */
+    }
 
+    void setUpOTC() {
+        // Check if the one time code corresponds to a child's UID and if it does, add that child to the doctors list of patients
+        submitOTCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String OTC = enterOTC.getText().toString();
+                DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+                dbRef.child("OTC's").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                            if (childSnapshot.getValue() == null) {
+                                continue;
+                            }
+                            else if (childSnapshot.getValue().equals(Long.parseLong(OTC))) {
+                                String patientUID = childSnapshot.getKey();
+                                dbRef.child("Users").child(UID).child("Patients").child(patientUID).setValue(true);
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+            }
+        });
     }
 }
