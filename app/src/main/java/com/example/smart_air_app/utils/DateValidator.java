bd3cdd09fb8b52d2527426bb3541629cdf338124
Alexander Chan <alexanderwc.chan@mail.utoronto.ca>
2025-11-29 21:28:48 -0500

    fix:fixed controller logging to work with notifications

--- app/src/main/java/com/example/smart_air_app/utils/DateValidator.java
+++ app/src/main/java/com/example/smart_air_app/utils/DateValidator.java
@@ -55,43 +55,43 @@ public class DateValidator {
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        return DateValidator.makeDateString(day, month, year);
-    }
-
-    public static String makeDateString(int day, int month, int year) {
-        if (day < 10) {
-            return DateValidator.getMonthFormat(month) + " " + "0" + day + " " + year;
+            month = month + 1;
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            return DateValidator.makeDateString(day, month, year);
         }
-        return DateValidator.getMonthFormat(month) + " " + day + " " + year;
-    }
 
-    public static String getMonthFormat(int month) {
-        if(month == 1)
+        public static String makeDateString(int day, int month, int year) {
+            if (day < 10) {
+                return DateValidator.getMonthFormat(month) + " " + "0" + day + " " + year;
+            }
+            return DateValidator.getMonthFormat(month) + " " + day + " " + year;
+        }
+
+        public static String getMonthFormat(int month) {
+            if(month == 1)
+                return "JAN";
+            if(month == 2)
+                return "FEB";
+            if(month == 3)
+                return "MAR";
+            if(month == 4)
+                return "APR";
+            if(month == 5)
+                return "MAY";
+            if(month == 6)
+                return "JUN";
+            if(month == 7)
+                return "JUL";
+            if(month == 8)
+                return "AUG";
+            if(month == 9)
+                return "SEP";
+            if(month == 10)
+                return "OCT";
+            if(month == 11)
+                return "NOV";
+            if(month == 12)
+                return "DEC";
             return "JAN";
-        if(month == 2)
-            return "FEB";
-        if(month == 3)
-            return "MAR";
-        if(month == 4)
-            return "APR";
-        if(month == 5)
-            return "MAY";
-        if(month == 6)
-            return "JUN";
-        if(month == 7)
-            return "JUL";
-        if(month == 8)
-            return "AUG";
-        if(month == 9)
-            return "SEP";
-        if(month == 10)
-            return "OCT";
-        if(month == 11)
-            return "NOV";
-        if(month == 12)
-            return "DEC";
-        return "JAN";
-    }
+        }
 }
