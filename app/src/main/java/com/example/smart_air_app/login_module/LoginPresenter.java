3c1107b70a08873446eee8d7261ef63e88a074f4
ericp <eric.proud11@gmail.com>
2025-12-01 23:29:01 -0500

    refactor: Commented the login module for others / TA to look over

--- app/src/main/java/com/example/smart_air_app/login_module/LoginPresenter.java
+++ app/src/main/java/com/example/smart_air_app/login_module/LoginPresenter.java
@@ -3,89 +3,104 @@ package com.example.smart_air_app.login_module;
 public class LoginPresenter {
     LoginModel model;
     LoginView view;
+
     public LoginPresenter(LoginModel model, LoginView view) {
         this.model = model;
         this.view = view;
     }
 
+    // Callback interface to be implemented by the view
     public interface AuthView {
-        void redirectToHome(String userType);
-        void showAuthFailedMessage();
-        void resetInputs();
-        void showError(String error);
-        void onEmptyUsernameOrEmail();
-        void onEmptyPassword();
+        void redirectToHome(String userType);   // Callback to redirect to the home screen
+
+        void showAuthFailedMessage();           // Callback to show the authentication failed message
+
+        void resetInputs();                     // Callback to reset the inputs to the form
+
+        void showError(String error);           // Callback to show an error message
+
+        void onEmptyUsernameOrEmail();          // Callback to handle empty username or email
+
+        void onEmptyPassword();                 // Callback to handle empty password
     }
 
     private class LoginCallback implements LoginModel.AuthCallback {
         @Override
-        public void onAuthSuccess(String userType) {
-            // store basic information about the user like id to be used to make later requests
-            view.redirectToHome(userType);
+        public void onAuthSuccess(String userType) {    // When login is successful
+            view.redirectToHome(userType);              // Redirect to the home page
         }
 
         @Override
-        public void onAuthFailure(String error) {
-            view.showAuthFailedMessage();
-            view.resetInputs();
-            view.showError(error);
+        public void onAuthFailure(String error) {   // When login fails
+            view.showAuthFailedMessage();           // Show default failure message
+            view.resetInputs();                     // Reset input fields
+            view.showError(error);                  // Show specific error message
         }
-    }
 
-    public void Login(String usernameOrEmail, String password) {
-        String handledUsernameOrEmail;
-        LoginCallback callback;
+        // login method takes username and password and will pass it to the model
+        public void Login(String usernameOrEmail, String password) {
+            String handledUsernameOrEmail;
+            LoginCallback callback;
 
-        if (!validateInputs(usernameOrEmail, password)) {
-            return;
+            if (!validateInputs(usernameOrEmail, password)) {   // If inputs are invalid
+                return;                                         // Do not continue (call to validateTnputs handles errors)
+            }
+            callback = new LoginCallback();     // Initialize the callback
+            handledUsernameOrEmail = convertUsername(usernameOrEmail);  // Convert username to email if needed
+            model.login(handledUsernameOrEmail, password, callback);    // Call the model's login method
         }
-        callback = new LoginCallback();
-        handledUsernameOrEmail = convertUsername(usernameOrEmail);
-        model.signIn(handledUsernameOrEmail, password, callback);
-    }
 
-    public boolean validateInputs(String usernameOrEmail, String password) {
-        boolean validUsernameOrEmail;
-        boolean validPassword;
+        // Ensures neither the email nor the password are vailid
+        public boolean validateInputs(String usernameOrEmail, String password) {
+            boolean validUsernameOrEmail;
+            boolean validPassword;
 
-        validUsernameOrEmail = validateUsernameOrEmail(usernameOrEmail);
-        validPassword = validatePassword(password);
+            validUsernameOrEmail = validateUsernameOrEmail(usernameOrEmail);    // Check if username / email is valid
+            validPassword = validatePassword(password);                         // Check if password is valid
 
-        if (!validateUsernameOrEmail(usernameOrEmail)) {
-            handleEmptyUsernameOrEmail();
+            if (!validUsernameOrEmail) {        // If username / email is invalid
+                handleEmptyUsernameOrEmail();   // Call the callback to handle the error
+            }
+            if (!validPassword) {               // If password is invalid
+                handleEmptyPassword();          // Call the callback to handle the error
+            }
+            return validUsernameOrEmail && validPassword;  // Return true iff ( username / email ) and password are valid
         }
-        if (!validPassword) {
-            handleEmptyPassword();
+
+        // Checks if username / email is valid
+        public boolean validateUsernameOrEmail(String usernameOrEmail) {
+            if (usernameOrEmail.isEmpty()) {    // If username / email is empty
+                return false;                   // Return false
+            }
+            return true;    // Otherwise return true
         }
-        return validUsernameOrEmail && validPassword;
-    }
 
-    public boolean validateUsernameOrEmail(String usernameOrEmail) {
-        if (usernameOrEmail.isEmpty()) {
-            return false;
+        // Checks if password is valid
+        public boolean validatePassword(String password) {
+            if (password.isEmpty()) {   // If password is empty
+                return false;           // Return false
+            }
+            return true;    // Otherwise return true
         }
-        return true;
-    }
 
-    public boolean validatePassword(String password) {
-        if (password.isEmpty()) {
-            return false;
+        // Handles the empty username / email error with a callback to the view
+        public void handleEmptyUsernameOrEmail() {
+            view.onEmptyUsernameOrEmail();  // Call the callback to handle the error
         }
-        return true;
-    }
 
-    public void handleEmptyUsernameOrEmail() {
-        view.onEmptyUsernameOrEmail();
-    }
-
-    public void handleEmptyPassword() {
-        view.onEmptyPassword();
-    }
-
-    public String convertUsername(String usernameOrEmail) {
-        if (usernameOrEmail.contains("@")) {
-            return usernameOrEmail;
+        // Handles the empty password error with a callback to the view
+        public void handleEmptyPassword() {
+            view.onEmptyPassword();         // Call the callback to handle the error
         }
-        else return usernameOrEmail + "@xyz.com";
+
+
+        // Convert childs username to an email for the purposes of the app as allowed on Piazza
+        // Lets us register an account for a child as though they had an email
+        public String convertUsername(String usernameOrEmail) {
+            if (usernameOrEmail.contains("@")) {    // If username / email is already an email
+                return usernameOrEmail;             // Return as is
+            } else
+                return usernameOrEmail + "@xyz.com";   // Otherwise, add fake email extension to end of username
+        }
     }
 }
--- app/src/main/java/com/example/smart_air_app/login_module/LoginView.java
+++ app/src/main/java/com/example/smart_air_app/login_module/LoginView.java
@@ -23,13 +23,13 @@ import com.example.smart_air_app.ResetPasswordScreen;
 import com.example.smart_air_app.StartScreen;
 
 public class LoginView extends AppCompatActivity implements LoginPresenter.AuthView {
+    LoginPresenter presenter;
     Button backButton;
     Button loginButton;
     Button recoverAccountButton;
     EditText inputUsernameOrEmail;
     EditText inputPassword;
     TextView authFailedText;
-    LoginPresenter presenter;
 
 
     @Override
@@ -42,83 +42,103 @@ public class LoginView extends AppCompatActivity implements LoginPresenter.AuthV
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        presenter = new LoginPresenter(new LoginModel(), this);     // Initialize presenter with a login model
+                                                                         // And the current view (this)
 
+        // Init buttons
         backButton = findViewById(R.id.buttonLoginBack);
         loginButton = findViewById(R.id.LoginButton);
+        recoverAccountButton = findViewById(R.id.buttonRecoverAccount);
+
+        // Init input fields
         inputUsernameOrEmail = findViewById(R.id.inputLoginUsernameOrEmail);
         inputPassword = findViewById(R.id.inputLoginPassword);
-        authFailedText = findViewById(R.id.AuthFailedText);
-        recoverAccountButton = findViewById(R.id.buttonRecoverAccount);
-        presenter = new LoginPresenter(new LoginModel(), this);
 
+        // Init popup text for if / when login fails and make it hidden at first
+        authFailedText = findViewById(R.id.AuthFailedText);
         authFailedText.setVisibility(View.INVISIBLE);
 
+        // Set on click listener for login button
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = inputUsernameOrEmail.getText().toString();
-                String password = inputPassword.getText().toString();
-                presenter.Login(email, password);
+                String email = inputUsernameOrEmail.getText().toString();   // Get inputted username / email
+                String password = inputPassword.getText().toString();       // Get inputted password
+                presenter.Login(email, password);   // Call the presenter's login method with the username / email and password
             }
         });
 
+        // Set on click listener for recover account button
         recoverAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginView.this, ResetPasswordScreen.class);
-                startActivity(intent);
+                Intent intent = new Intent(LoginView.this, ResetPasswordScreen.class);  // Create intent to go to reset password screen
+                startActivity(intent);  // Send user to reset password screen
+                finish();   // Terminate current activity
             }
         });
 
+        // Set on click listener for back button
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginView.this, StartScreen.class);
-                startActivity(intent);
+                Intent intent = new Intent(LoginView.this, StartScreen.class);  // Create intent to go to start screen
+                startActivity(intent);  // Send user to start screen
+                finish();   // Terminate current activity
             }
         });
     }
+
+    // Callback method for when login is successful to redirect to the home screen
+
     @Override
     public void redirectToHome(String userType) {
         Intent intent;
-
-        if (userType.equals("child")) {
-            intent = new Intent(LoginView.this, ChildHomeScreen.class);
+        if (userType.equals("child")) {         // If user is a child
+            intent = new Intent(LoginView.this, ChildHomeScreen.class);     // Create intent to go to child home screen
         }
-        else if (userType.equals("parent")) {
-            intent = new Intent(LoginView.this, ParentHomeScreen.class);
+        else if (userType.equals("parent")) {   // If user is a parent
+            intent = new Intent(LoginView.this, ParentHomeScreen.class);    // Create intent to go to parent home screen
         }
-        else if (userType.equals("doctor")) {
-            intent = new Intent(LoginView.this, DoctorHomeScreen.class);
+        else if (userType.equals("doctor")) {   // If user is a doctor
+            intent = new Intent(LoginView.this, DoctorHomeScreen.class);    // Create intent to go to doctor home screen
         }
-        else intent = new Intent(LoginView.this, StartScreen.class);
-        startActivity(intent);
-        finish();
+        else intent = new Intent(LoginView.this, StartScreen.class);        // Otherwise, something has gone wrong,
+                                                                                          // Create intent to go to start screen
+        startActivity(intent);      // Send user to their respective home screen
+        finish();                   // Terminate current activity
     }
 
+    // Callback method for when login fails to show the default failure message
     @Override
     public void showAuthFailedMessage() {
-        authFailedText.setVisibility(View.VISIBLE);
+        authFailedText.setVisibility(View.VISIBLE);     // Make the popup failure text visible
     };
 
+    // Callback method for when login fails / an input field is empty to reset the input fields
     @Override
     public void resetInputs(){
-        inputUsernameOrEmail.setText("");
-        inputPassword.setText("");
+        inputUsernameOrEmail.setText("");   // Reset the username / email input field
+        inputPassword.setText("");          // Reset the password input field
     }
 
+    // Callback method for when login fails to show an error message
     @Override
     public void showError(String error) {
-        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();  // Show the error message
     };
 
+    // Callback method for when the username / email is invalid
     @Override
     public void onEmptyUsernameOrEmail() {
+        // Set the hint text colour to red
         inputUsernameOrEmail.setHintTextColor(getResources().getColor(android.R.color.holo_red_dark));
     }
 
+    // Callback method for when the password is invalid
     @Override
     public void onEmptyPassword() {
+        // Set the hint text colour to red
         inputPassword.setHintTextColor(getResources().getColor(android.R.color.holo_red_dark));
     }
 }
